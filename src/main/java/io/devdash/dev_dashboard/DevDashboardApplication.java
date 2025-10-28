90e5b29d74280c228c0ec16bac20d19dcc5af5e2
Adis <priv4484@gmail.com>
2025-10-28 20:41:41 +0500

    Initial commit

---
+++ src/main/java/io/devdash/dev_dashboard/DevDashboardApplication.java
@@ -0,0 +1,13 @@
+package io.devdash.dev_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/devdash/dev_dashboard/DevDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devdash.dev_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
